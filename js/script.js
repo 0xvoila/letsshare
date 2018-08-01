02a93798038e0e7fbc4d41c74c4b344e41e8b18b
Admin <Admin@Admins-MacBook-Pro.local>
2018-08-01 20:35:44 +0530

    Now comments are loading from algolia .. all done

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ $( document ).ready(function() {
                 $.ajax({
                     type: 'get',
                     headers: {"X-Algolia-API-Key": "7b0cab452409affbc3e9cdd8dd6260e1", "X-Algolia-Application-Id" :"WEQ1ZSOQ0G" },
-                    url: 'https://WEQ1ZSOQ0G.algolia.net/1/indexes/deal_search/' + 481590640 + '?attributes=*',
+                    url: 'https://WEQ1ZSOQ0G.algolia.net/1/indexes/deal_search/' + deal.id + '?attributes=*',
                     success: function(dealObject) {
                         success(dealObject.comments)
                     },
@@ -55,7 +55,7 @@ $( document ).ready(function() {
                 $.ajax({
                     type: 'post',
                     headers: {"X-Algolia-API-Key": "385f901dcaf5f9c89672ba880f4b5eab", "X-Algolia-Application-Id" :"WEQ1ZSOQ0G" },
-                    url: 'https://WEQ1ZSOQ0G.algolia.net/1/indexes/deal_search/' + 481590640 + '/partial',
+                    url: 'https://WEQ1ZSOQ0G.algolia.net/1/indexes/deal_search/' + deal.id + '/partial',
                     data: JSON.stringify(requestData),
                     success: function(comment) {
                         success(commentJSON)
@@ -94,7 +94,7 @@ $( document ).ready(function() {
                             }
                         
                         
-		  				htmlTemplate = htmlTemplate + '<!-- A row is a deal --><div class="row deal" ><!-- This is  pic of the poster -->  <div class="col-sm-2 deal-poster-image">      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRErG00hM7WDheP9FCZvZTIWGurbuKLDCAoHAmKKemK2s6vsLjA" width="100%" height="100%">  </div><!-- This is deal portion -->  <div class="col-sm-10"> <!-- This is to show title and Code and share a deal button-->    <div class="row">      <div class="col-sm-8 deal-title"><button type="button" class="btn-xs btn-success">verified</button>' +        '<h3><a href="#">'+ records[i].deal_title +'</a></h3>      </div>      <div class="col-sm-2 deal-coupon">        <span class="deal-coupon-code">' + records[i].deal_coupon + '</span>      </div>    </div>    <!-- This is to show description --> <div class="row"><div class="col-sm-8">' + '<a href="' + records[i].deal_url + '">' + records[i].deal_url + '</a>' + '</div></div>   <div class="row">      <div class="col-sm-8 deal-description">' +        records[i].deal_description    + '</div>    </div><br><br>    <div class="row">      <div class="col-md-8"> <div id="comments-container"> </div></div>  </div>       </div>    </div>  </div></div><hr>';
+		  				htmlTemplate = htmlTemplate + '<!-- A row is a deal --><div id=' + records[i].objectID + ' class="row deal" ><!-- This is  pic of the poster -->  <div class="col-sm-2 deal-poster-image">      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRErG00hM7WDheP9FCZvZTIWGurbuKLDCAoHAmKKemK2s6vsLjA" width="100%" height="100%">  </div><!-- This is deal portion -->  <div class="col-sm-10"> <!-- This is to show title and Code and share a deal button-->    <div class="row">      <div class="col-sm-8 deal-title"><button type="button" class="btn-xs btn-success">verified</button>' +        '<h3><a href="#">'+ records[i].deal_title +'</a></h3>      </div>      <div class="col-sm-2 deal-coupon">        <span class="deal-coupon-code">' + records[i].deal_coupon + '</span>      </div>    </div>    <!-- This is to show description --> <div class="row"><div class="col-sm-8">' + '<a href="' + records[i].deal_url + '">' + records[i].deal_url + '</a>' + '</div></div>   <div class="row">      <div class="col-sm-8 deal-description">' +        records[i].deal_description    + '</div>    </div><br><br>    <div class="row">      <div class="col-md-8"> <div id="comments-container"> </div></div>  </div>       </div>    </div>  </div></div><hr>';
 		  			};
 
 		  			$("#deal-container").html(htmlTemplate);
